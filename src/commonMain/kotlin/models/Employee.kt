bd68d309aa4c7b3184c7ecb8de013bb5186dcc42
fps420 <schoenknecht.florian@gmail.com>
2021-08-02 14:11:56 +0200

    Use Role in Employee.kt

--- src/commonMain/kotlin/models/Employee.kt
+++ src/commonMain/kotlin/models/Employee.kt
@@ -2,4 +2,4 @@ package models
 
 import com.benasher44.uuid.Uuid
 
-data class Employee(val employeeId: Uuid, val employeeRole: String )
+data class Employee(val employeeId: Uuid, val employeeRole: Role )
