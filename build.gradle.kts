dd22cbebe5a55cba9fd7eca50d05122d2b58e8e4
fps420 <schoenknecht.florian@gmail.com>
2021-08-05 09:40:04 +0200

    Create basic feature to r, w, x with cloud database mongodb

--- build.gradle.kts
+++ build.gradle.kts
@@ -65,7 +65,8 @@ kotlin {
                 implementation("io.ktor:ktor-server-core:$ktorVersion")
                 implementation("io.ktor:ktor-server-netty:$ktorVersion")
                 implementation("ch.qos.logback:logback-classic:$logbackVersion")
-                implementation("org.litote.kmongo:kmongo-coroutine-serialization:$kmongoVersion")
+                implementation("org.litote.kmongo:kmongo:$kmongoVersion")
+
             }
         }
 
--- src/jvmMain/kotlin/server.kt
+++ src/jvmMain/kotlin/server.kt
@@ -1,18 +1,35 @@
+import com.benasher44.uuid.Uuid
+import com.mongodb.ConnectionString
+import com.mongodb.MongoClientSettings
 import io.ktor.application.*
 import io.ktor.features.*
 import io.ktor.http.*
-import io.ktor.routing.get
-import io.ktor.routing.routing
-import io.ktor.server.engine.embeddedServer
-import io.ktor.server.netty.Netty
-import io.ktor.http.content.resources
-import io.ktor.http.content.static
+import io.ktor.http.content.*
 import io.ktor.response.*
+import io.ktor.routing.*
 import io.ktor.serialization.*
+import io.ktor.server.engine.*
+import io.ktor.server.netty.*
+import models.Person
+import org.litote.kmongo.*
+import java.util.*
 
 
 fun main() {
+    val connectionString =
+        ConnectionString("mongodb+srv://prowhiz:0co8nr3Z29Wv5XbQ@orsecluster.ops2n.mongodb.net/myFirstDatabase?retryWrites=true&w=majority")
+    val settings = MongoClientSettings.builder()
+        .applyConnectionString(connectionString)
+        .build()
+
+
+    val client = KMongo.createClient(settings)
+    val database = client.getDatabase("orsedatabase")
+    val col = database.getCollection<Person>()
+
+
     embeddedServer(Netty, port = 8080, host = "127.0.0.1") {
+
         routing {
             install(ContentNegotiation) {
                 json()
@@ -28,10 +45,11 @@ fun main() {
             }
 
             get("/") {
-                call.respondText(
-                    this::class.java.classLoader.getResource("index.html")!!.readText(),
-                    ContentType.Text.Html
-                )
+
+            }
+            post("/") {
+                col.insertOne(Person("firstname","lastname", Uuid.randomUUID()))
+                call.respond(HttpStatusCode.OK)
             }
             static("/") {
                 resources("")
@@ -39,3 +57,6 @@ fun main() {
         }
     }.start(wait = true)
 }
+
+
+
