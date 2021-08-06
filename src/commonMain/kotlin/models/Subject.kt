90ae43e2bb2d2bff3274a872832c11b2d89cb7e4
fps420 <schoenknecht.florian@gmail.com>
2021-08-06 14:20:00 +0200

    Update Booking

--- src/commonMain/kotlin/models/Subject.kt
+++ src/commonMain/kotlin/models/Subject.kt
@@ -1,6 +1,5 @@
 package models
 
-import com.benasher44.uuid.Uuid
 import kotlinx.serialization.Serializable
 
 @Serializable
--- src/jvmMain/kotlin/routing/BookingRoutes.kt
+++ src/jvmMain/kotlin/routing/BookingRoutes.kt
@@ -1,14 +1,18 @@
 package routing
 
 import databaseService
+import de.undercouch.bson4jackson.types.ObjectId
 import io.ktor.application.*
 import io.ktor.http.*
 import io.ktor.request.*
 import io.ktor.response.*
 import io.ktor.routing.*
-import kotlinx.serialization.decodeFromString
+import kotlinx.serialization.encodeToString
 import kotlinx.serialization.json.Json
 import models.Booking
+import org.litote.kmongo.eq
+import org.litote.kmongo.findOne
+import org.litote.kmongo.findOneById
 
 fun Route.bookingRoutes() {
     post("/booking") {
@@ -22,7 +26,15 @@ fun Route.bookingRoutes() {
         }
         call.respond(HttpStatusCode.OK)
     }
-    get("/booking") {
-        call.respond(HttpStatusCode.OK)
+    get("/booking/all") {
+        try {
+            val col = databaseService.getCollectionOfBooking()
+            val booking = col.find()
+
+            call.respondText(Json.encodeToString(booking))
+            call.respond(HttpStatusCode.OK)
+        } catch (e: Exception) {
+            call.respondText("Error_ $e")
+        }
     }
 }
