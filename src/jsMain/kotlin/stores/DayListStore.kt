cc11b99d98c229fab2d9b1a7037c0227587c7e70
fps420 <schoenknecht.florian@gmail.com>
2021-08-19 14:37:03 +0200

    Implement interactive Calendar UI

--- src/jsMain/kotlin/stores/DayListStore.kt
+++ src/jsMain/kotlin/stores/DayListStore.kt
@@ -3,6 +3,8 @@ package stores
 import dev.fritz2.binding.RootStore
 import dev.fritz2.identification.uniqueId
 import dev.fritz2.repositories.rest.restQuery
+import kotlinx.datetime.LocalDate
+import kotlinx.datetime.toLocalDate
 import models.Day
 import models.DayResource
 
@@ -14,11 +16,19 @@ object DayListStore : RootStore<List<Day>>(emptyList()) {
     val query = handle {
         repo.query(Unit)
     }
+    val getDay = handleAndEmit<LocalDate, Day> { model, date ->
+        val day = model.filter {
+            it.day.toLocalDate() == date
+        }.first()
+        emit(day)
+        model
+    }
 
     init {
         query()
         SubjectListStore.save handledBy query
         BookingListStore.save handledBy query
+        getDay handledBy SelectedDayStore.update
     }
 }
 
