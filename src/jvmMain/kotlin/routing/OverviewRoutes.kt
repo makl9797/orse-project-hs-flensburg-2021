6f8d928d4cd788f91004b9783e6e3cca2a56e501
fps420 <schoenknecht.florian@gmail.com>
2021-09-07 13:13:55 +0200

    Commenting OverviewRoutes.kt

--- src/jvmMain/kotlin/routing/OverviewRoutes.kt
+++ src/jvmMain/kotlin/routing/OverviewRoutes.kt
@@ -10,8 +10,16 @@ import models.data.Booking
 import models.data.Day
 import models.data.Subject
 
-
+/**
+ * Here is the implementation of the Routes of Overview
+ */
 fun Route.overviewRoutes() {
+
+    /**
+     * get(/overview) delivers a List of Days upto "period",
+     * containing the bookings and available subjects of each day.
+     * If something went wrong we catch it and send a report
+     */
     get("/overview") {
         try {
             var period = call.parameters["periodInDays"]?.toInt()
@@ -29,18 +37,22 @@ fun Route.overviewRoutes() {
     }
 }
 
+/**
+ * createDayList @returns a mutable list of days containing available subjects und bookings of each day
+ */
 fun createDayList(period: Int): MutableList<Day> {
-    val today = Clock.System.todayAt(TimeZone.currentSystemDefault())
-    val endOfTimeFrame = today.plus(DatePeriod(days = period))
-    val bookingsInTimeframe = getBookingsInTimeframe(today, endOfTimeFrame)
-    val daylist = mutableListOf<Day>()
-    val allSubjects = getAllSubjects()
+    val today = Clock.System.todayAt(TimeZone.currentSystemDefault())       // get current date
+    val endOfTimeFrame = today.plus(DatePeriod(days = period))              // get last day of timeframe
+    val bookingsInTimeframe = getBookingsInTimeframe(today, endOfTimeFrame) // get bookings in the current timeframe
+    val daylist = mutableListOf<Day>()                                      // create an empty list of days
+    val allSubjects = getAllSubjects()                                      // get a list of all subjects
     for (i in 0..period) {
-        val nextDay = today.plus((DatePeriod(days = i)))
-        val bookingsOfDay = getBookingsOfDay(nextDay, bookingsInTimeframe)
-        val availableSubjects = getAvailableSubjectsOfDay(allSubjects.toMutableList(), bookingsOfDay)
+        val nextDay = today.plus((DatePeriod(days = i)))                    // get the next
+        val bookingsOfDay = getBookingsOfDay(nextDay, bookingsInTimeframe)  // get bookings of a day
+        val availableSubjects =
+            getAvailableSubjectsOfDay(allSubjects.toMutableList(), bookingsOfDay) // get available subjects of a day
 
-        daylist.add(
+        daylist.add( // create a Day
             Day(
                 nextDay.toString(),
                 bookingsOfDay,
@@ -52,7 +64,9 @@ fun createDayList(period: Int): MutableList<Day> {
     return daylist
 }
 
-
+/**
+ * @return a list of Bookings on a specific day
+ */
 fun getBookingsOfDay(day: LocalDate, bookingsInTimeframe: List<Booking>): MutableList<Booking> {
     val bookingsOnDay = mutableListOf<Booking>()
     bookingsInTimeframe.forEach { booking ->
@@ -65,6 +79,9 @@ fun getBookingsOfDay(day: LocalDate, bookingsInTimeframe: List<Booking>): Mutabl
     return bookingsOnDay
 }
 
+/**
+ * @return a list of Bookings in a given timeframe
+ */
 fun getBookingsInTimeframe(start: LocalDate, end: LocalDate): List<Booking> {
     val allBookings = databaseService.getCollectionOfBooking().find().toList()
     val bookingsInTimeFrame = mutableListOf<Booking>()
@@ -75,11 +92,16 @@ fun getBookingsInTimeframe(start: LocalDate, end: LocalDate): List<Booking> {
     return bookingsInTimeFrame
 }
 
+/**
+ * @return a list of all subjects
+ */
 fun getAllSubjects(): List<Subject> {
     return databaseService.getCollectionOfSubject().find().toList()
 }
 
-
+/**
+ * @return a list of subjects that are available at a specific day
+ */
 fun getAvailableSubjectsOfDay(
     subjects: MutableList<Subject>,
     bookingsOfDay: MutableList<Booking>
