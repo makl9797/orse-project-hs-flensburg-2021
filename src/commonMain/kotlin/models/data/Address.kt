eaa4e04857b6aff0ad46a7b2615dfac7ef726d90
fps420 <schoenknecht.florian@gmail.com>
2021-09-07 12:46:03 +0200

    Commenting Address.kt

--- src/commonMain/kotlin/models/data/Address.kt
+++ src/commonMain/kotlin/models/data/Address.kt
@@ -4,6 +4,9 @@ import dev.fritz2.identification.uniqueId
 import dev.fritz2.lenses.Lenses
 import kotlinx.serialization.Serializable
 
+/**
+ * This data class acts as Address, every Address has a unique id
+ */
 @Lenses
 @Serializable
 data class Address(
