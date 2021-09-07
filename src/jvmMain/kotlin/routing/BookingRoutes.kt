6bcac47176d63f0d59b93a3282945f9a9403ff74
fps420 <schoenknecht.florian@gmail.com>
2021-09-07 12:34:56 +0200

    Commenting BookingRoutes.kt

--- src/jvmMain/kotlin/routing/BookingRoutes.kt
+++ src/jvmMain/kotlin/routing/BookingRoutes.kt
@@ -10,63 +10,74 @@ import models.data.Booking
 import org.litote.kmongo.eq
 import org.litote.kmongo.findOneById
 import org.litote.kmongo.save
-// Here is the implementation of the Routes of Booking
+
+/**
+ * Here is the implementation of the Routes of Booking
+ */
 fun Route.bookingRoutes() {
-    // delivers every booking on a specific day
+    /**
+     * get(/bookings) delivers every booking on a specific day
+     */
     get("/bookings") {
         try {
             val col = databaseService.getCollectionOfBooking()  // get collection of Booking
             val day = call.request.queryParameters["day"]       // extract day by parameters
-            var bookings = col.find().toList()
+            var bookings = col.find().toList()                  // get a list of all bookings
             if (day != null) {
-                bookings = col.find(Booking::startTime eq day).toList()
+                bookings = col.find(Booking::startTime eq day).toList() // filter bookings by day
             }
-            call.respond(bookings)
-        } catch (e: Exception) {
+            call.respond(bookings)                              // respond the result
+        } catch (e: Exception) {                                // if something went wrong we catch it and respond report
             call.response.status(HttpStatusCode.BadRequest)
             call.respondText("Error_ $e")
         }
     }
-    // delivers a booking by id
+    /**
+     * get(bookings/id) delivers a booking by id
+     */
     get("/bookings/{id}") {
         try {
-            val col = databaseService.getCollectionOfBooking()
-            val id = call.parameters["id"].toString()
-            val booking = col.findOneById(id)
+            val col = databaseService.getCollectionOfBooking()   // get collection of Booking
+            val id = call.parameters["id"].toString()            // get the transmitted id
+            val booking = col.findOneById(id)                   // find the booking with by id
             if (booking != null) {
-                call.respond(booking)
+                call.respond(booking)                           // respond the result
             } else {
                 call.respondText("Booking with _id:$id not found")
             }
-        } catch (e: Exception) {
+        } catch (e: Exception) {                                 // if something went wrong we catch it and respond report
             call.response.status(HttpStatusCode.BadRequest)
             call.respondText("Error_ $e")
         }
     }
-    // create or update a booking by id
+    /**
+     * put(bookings/id) create or update a booking by id
+     */
     put("/bookings/{id}") {
         try {
-            val booking = call.receive<Booking>()
-            val bookingId = call.parameters["id"].toString()
-            booking._id = bookingId
-            val col = databaseService.getCollectionOfBooking()
-            col.save(booking)
+            val booking = call.receive<Booking>()               // expect a Booking
+            val bookingId = call.parameters["id"].toString()    // get the transmitted id
+            booking._id = bookingId                             // set transmitted id to booking id
+            val col = databaseService.getCollectionOfBooking()  // get collection of Booking
+            col.save(booking)                                   // save the booking in collection
 
-            call.respond(col.find().toList())
+            call.respond(col.find().toList())                   // respond all bookings
 
-        } catch (e: Exception) {
+        } catch (e: Exception) {                                 // if something went wrong we catch it and respond report
             call.response.status(HttpStatusCode.BadRequest)
             call.respondText("Error_ $e")
         }
     }
-    // delete a booking by id
+    /**
+     * delete(bookins/id) delete a booking by id
+     */
     delete("/bookings/{id}") {
         try {
-            val col = databaseService.getCollectionOfBooking()
-            col.deleteOne(Booking::_id eq call.parameters["id"])
-            call.respond(col.find().toList())
+            val col = databaseService.getCollectionOfBooking()          // get collection of Booking
+            col.deleteOne(Booking::_id eq call.parameters["id"])   // delete Booking with the given id
+            call.respond(col.find().toList())                           // respond all bookings
 
-        } catch (e: Exception) {
+        } catch (e: Exception) {                                // if something went wrong we catch it and respond report
             call.response.status(HttpStatusCode.BadRequest)
             call.respondText("Error_ $e")
         }
